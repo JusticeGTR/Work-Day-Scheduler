ccd47756a3b556b2a2d985da3b22e2a99f0178c5
Justin Lindsey <justinlindseylhr@gmail.com>
2021-04-09 21:23:57 -0500

    updating

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Work-Day-Scheduler
+
+This is our fifth homework assignment in nu-chi-fsf-ft-03-2021-u-c! 
+
+In this excercise, we were tasked with creating 
+
+Acceptance criteria:
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+
+
+The final page:
+
+
+## Visuals
+![Portfolio webpage]()
+
+## Usage
+
+
+
+## Contributing
+
+Feel free to contribute by suggesting better code! Contributing a good grade is also welcome.
+
+## Lincensing
+
+Justin Lindsey & Co. 2021
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
 function displayTime() {
-    var time = moment().format('dddd, MMMM Do YYYY, hh:mm:ss a');
-    $('#currentDay').text(time);
+    var time = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a");
+    $("#currentDay").text(time);
     setTimeout(displayTime, 1000);
+    // return time
 }
 
 $(document).ready(function() {
     displayTime();
+
+    // return time
 });
+
+// function colorChanger(time) {
+//     var nineAm = $("#nineAm").moment("dddd, MMMM Do YYYY, 09:mm:ss a")
+//     var tenAm = $("#tenAm")
+//     var elevenAm = $("#elevenAm")
+//     var twelveAm = $("#twelveAm")
+//     var onePm = $("#onePm")
+//     var twoPm = $("#twoPm")
+//     var threePm = $("#threePm")
+//     var fourPm = $("#fourPm")
+//     var fivePm = $("#fivePm")
+//     var colorTime = [nineAm, tenAm, elevenAm, twelveAm, onePm, twoPm, threePm, fourPm, fivePm] 
+//     // var colorTime = [$("#09"), $("#10"), $("#11"), $("#12"), $("#13"), $("#14"), $("#15"), $("#16"), $("#17")] 
+  
+
+//     var hourColor = moment().format("hh")
+//     console.log(hourColor);
+//     for(var i = 0; i <colorTime.length; i++) {
+//         if(colorTime[i] === time) {
+//             colorTime[i].
+//         }
+//     }
+// }
+
+// colorChanger()
