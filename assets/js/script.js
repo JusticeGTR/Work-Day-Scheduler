129c7d8037dbfca6e29aa07ab62b22f813ad2adf
Justin Lindsey <justinlindseylhr@gmail.com>
2021-04-11 14:38:47 -0500

    trying out new things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,40 @@
+var time = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a");
+
 function displayTime() {
-    var time = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a");
+    time = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a");
     $("#currentDay").text(time);
     setTimeout(displayTime, 1000);
-    // return time
 }
-
 $(document).ready(function() {
     displayTime();
-
-    // return time
 });
 
-// function colorChanger(time) {
-//     var nineAm = $("#nineAm").moment("dddd, MMMM Do YYYY, 09:mm:ss a")
-//     var tenAm = $("#tenAm")
-//     var elevenAm = $("#elevenAm")
-//     var twelveAm = $("#twelveAm")
-//     var onePm = $("#onePm")
-//     var twoPm = $("#twoPm")
-//     var threePm = $("#threePm")
-//     var fourPm = $("#fourPm")
-//     var fivePm = $("#fivePm")
-//     var colorTime = [nineAm, tenAm, elevenAm, twelveAm, onePm, twoPm, threePm, fourPm, fivePm] 
-//     // var colorTime = [$("#09"), $("#10"), $("#11"), $("#12"), $("#13"), $("#14"), $("#15"), $("#16"), $("#17")] 
-  
+function colorChanger() {
+    var nineAm = $("#nineAm").moment("dddd, MMMM Do YYYY, 09:mm:ss am")
+    var tenAm = $("#tenAm").moment("dddd, MMMM Do YYYY, 10:mm:ss am")
+    var elevenAm = $("#elevenAm").moment("dddd, MMMM Do YYYY, 11:mm:ss am")
+    var twelveAm = $("#twelveAm").moment("dddd, MMMM Do YYYY, 12:mm:ss am")
+    var onePm = $("#onePm").moment("dddd, MMMM Do YYYY, 01:mm:ss pm")
+    var twoPm = $("#twoPm").moment("dddd, MMMM Do YYYY, 02:mm:ss pm")
+    var threePm = $("#threePm").moment("dddd, MMMM Do YYYY, 03:mm:ss pm")
+    var fourPm = $("#fourPm").moment("dddd, MMMM Do YYYY, 04:mm:ss pm")
+    var fivePm = $("#fivePm").moment("dddd, MMMM Do YYYY, 05:mm:ss pm")
+    var colorTime = [nineAm, tenAm, elevenAm, twelveAm, onePm, twoPm, threePm, fourPm, fivePm] 
+    console.log(colorTime)
+    
+    var hourColor = moment().format("hh")
+    console.log(hourColor);
+    for(var i = 0; i <colorTime.length; i++) {
+        while(colorTime[i] > time) {
+            colorTime[i].addClass("past")
+        }
+        while(colorTime[i] === time) {
+            colorTime[i].addClass("present")
+        }
+        while(colorTime[i] < time) {
+            colorTime[i].addClass("future")
+        }
+    }
+}
 
-//     var hourColor = moment().format("hh")
-//     console.log(hourColor);
-//     for(var i = 0; i <colorTime.length; i++) {
-//         if(colorTime[i] === time) {
-//             colorTime[i].
-//         }
-//     }
-// }
-
-// colorChanger()
+// colorChanger();
